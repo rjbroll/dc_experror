6550a243856b6e68613fb40c65482db4cf0a3b46
rjbroll <rjbroll16@gmail.com>
2025-03-14 21:19:14 -0400

    it's working now

--- code/old_code/odds.m
+++ code/old_code/odds.m
@@ -1,10 +1,41 @@
-function binaryresult = odds(X,Y,Z,alphatest,betatest)
-    odds1 = Y.*exp(-(alphatest + betatest*X)) - (1-Y);
-    odds2 = (1-Y).*(exp(alphatest + betatest*X)) - Y;
-    check = zeros(4,1);
-    check(1) = mean(odds1(Z==0)) >= 0 ;
-    check(2) = mean(odds1(Z==1)) >= 0 ;
-    check(3) = mean(odds2(Z==0)) >= 0 ;
-    check(4) = mean(odds2(Z==1)) >= 0 ;
-    binaryresult = sum(check) == 4;
+function mv = odds(alpha, beta, wval, xstarsupp)
+    % Z=0 
+    xstar = xstarsupp(1:2);
+    w = wval(1);
+
+    y_0 = w*Logit(alpha+beta*xstar(1)) + (1-w)*Logit(alpha+beta*xstar(2));
+    ye_0 = w*exp(-(alpha+beta*xstar(1)))*Logit(alpha+beta*xstar(1)) ...
+        + (1-w)*exp(-(alpha+beta*xstar(2)))*Logit(alpha+beta*xstar(2));
+    omye_0 = w*exp(alpha+beta*xstar(1))*(1-Logit(alpha+beta*xstar(1))) ...
+        + (1-w)*exp(alpha+beta*xstar(2))*(1-Logit(alpha+beta*xstar(2)));
+    mv1_0 = ye_0 - (1-y_0);
+    mv2_0 = omye_0 - y_0;
+    
+    % Z=1
+    xstar = xstarsupp(3:4);
+    w = wval(2);
+    y_1 = w*Logit(alpha+beta*xstar(1)) + (1-w)*Logit(alpha+beta*xstar(2));
+    ye_1 = w*exp(-(alpha+beta*xstar(1)))*Logit(alpha+beta*xstar(1)) ...
+        + (1-w)*exp(-(alpha+beta*xstar(2)))*Logit(alpha+beta*xstar(2));
+    omye_1 = w*exp(alpha+beta*xstar(1))*(1-Logit(alpha+beta*xstar(1))) ...
+        + (1-w)*exp(alpha+beta*xstar(2))*(1-Logit(alpha+beta*xstar(2)));
+    mv1_1 = ye_1 - (1-y_1);
+    mv2_1 = omye_1 - y_1;
+  
+    % Take minimum over both Z values and both inequalities
+    mv = min([mv1_0, mv2_0, mv1_1, mv2_1]);
 end
+
+
+
+
+% function binaryresult = odds(X,Y,Z,alphatest,betatest)
+%     odds1 = Y.*exp(-(alphatest + betatest*X)) - (1-Y);
+%     odds2 = (1-Y).*(exp(alphatest + betatest*X)) - Y;
+%     check = zeros(4,1);
+%     check(1) = mean(odds1(Z==0)) >= 0 ;
+%     check(2) = mean(odds1(Z==1)) >= 0 ;
+%     check(3) = mean(odds2(Z==0)) >= 0 ;
+%     check(4) = mean(odds2(Z==1)) >= 0 ;
+%     binaryresult = sum(check) == 4;
+% end
--- code/sharpidsets_analytical.m
+++ code/sharpidsets_analytical.m
@@ -1,59 +1,101 @@
-% 1. Set DGP
+%% 1. Set DGP
 alpha0 = 0;
 beta0 = 2;
-w = [55/70 15/70];
-xstarsupp = [.1 .8 .2 .9];
+w = [.5 .5];
+xstarsupp = [.1 .4 0 .1];
     % Calculate c for Z=0 and Z=1
 cmat = [c(xstarsupp,w,alpha0,beta0,0) c(xstarsupp,w,alpha0,beta0,1)];
     % Calculate znorm0 = E[X|Z=0] and znorm1 = E[X|Z=1]
 znorm = [w(1)*xstarsupp(1)+(1-w(1))*xstarsupp(2) w(2)*xstarsupp(3)+(1-w(2))*xstarsupp(4)];
 
-%%
-
-
-% 2. Iterate over directions in (alpha, beta) space 
+%% 2. Iterate over directions in (alpha, beta) space 
 thetaincrement = .01;
-thetagrid = 0:thetaincrement:2*pi;
-results = zeros(length(thetagrid),2);
+thetagrid = 0:thetaincrement:2*pi ;
+results = zeros(length(thetagrid),1);
 for i=1:length(thetagrid)
     theta = thetagrid(i);
-    fundist = @(r) paramboundist(r,alpha0,beta0,theta,cmat,znorm,0);
-    results(i,1) = fzero(fundist, 0); % remember the .1 is ad hoc for now
-    fundist = @(r) paramboundist(r,alpha0,beta0,theta,cmat,znorm,1);
-    results(i,2) = fzero(fundist, 0); % remember the .1 is ad hoc for now
-    disp(i);
+    disp(theta);
+    fundist = @(r) paramboundist(r,alpha0,beta0,theta,cmat,znorm);
+    results(i) = fzero(fundist, [0,1]); % remember the end value is ad hoc for now
 end
 
-%% 
-% 3. Take min over Z=0 and Z=1 boundaries and plot
-paramboundary = min(results,[],2);
+%% 3. Plot
+plotx = results .* cos(thetagrid') + alpha0;
+ploty = results .* sin(thetagrid') + beta0;
 
-%%
-function dist = paramboundist(r, alpha0, beta0, theta, cmat, znorm, zval)
-    alpha = alpha0 + abs(r)*cos(theta);
-    beta = beta0 + abs(r)*sin(theta);
+
+%% 4. Odds set
+oddsresults = zeros(length(thetagrid),1);
+for i = 1:length(thetagrid)
+    theta=thetagrid(i);
+    if odds(alpha0,beta0,.2,theta,w,xstarsupp) < 0
+        oddsfun = @(r) odds(alpha0,beta0,r,theta,w,xstarsupp);
+        oddsresults(i) = fzero(oddsfun,[0,.2]);
+    else 
+        oddsresults(i) = missing;
+    end
+end 
+
+
+oddsplotx = oddsresults .* cos(thetagrid') + alpha0;
+oddsploty = oddsresults .* sin(thetagrid') + beta0;
+
+
+
+%% 4. Bounding set
+boundresults = zeros(length(thetagrid),1);
+for i = 1:length(thetagrid)
+    theta=thetagrid(i);
+    if bounding(alpha0,beta0,1,theta,w,xstarsupp) < 0
+        boundfun = @(r) bounding(alpha0,beta0,r,theta,w,xstarsupp);
+        boundresults(i) = fzero(boundfun,[0,1]);
+    else 
+        boundresults(i) = missing;
+    end
+end 
+
+boundplotx = boundresults .* cos(thetagrid') + alpha0;
+boundploty = boundresults .* sin(thetagrid') + beta0;
+
+%% plot
+
+scatter(boundplotx, boundploty,8)
+hold on
+% scatter(oddsplotx, oddsploty,8)
+scatter(plotx, ploty, 8)
+hold off
+
+
+
+
+%% Functions
+
+function dist = paramboundist(r, alpha0, beta0, theta, cmat, znorm)
+    alpha = alpha0 + r*cos(theta);
+    beta = beta0 + r*sin(theta);
     % Calculate distance to edge of admissible moment set
-    z=zval;
-    tmin = min([-beta*znorm(z+1) beta*(1-znorm(z+1))]) ;
-    tmax = max([-beta*znorm(z+1) beta*(1-znorm(z+1))]);
-    alpha_z = alpha + beta*znorm(z+1);
-    c_x = cmat(1,z+1);
-        % Find left x-value of boundary
-    funneg = @(t) fneg(t, alpha, beta, cmat, znorm, tmax, z);
-    left_tval = fzero(funneg, [tmin,0]);
-    left_xval = (tmax/(tmax-left_tval))*Logit(alpha_z + left_tval) - ...
+    distvec = zeros(2);
+    for z=0:1
+        tmin = min([-beta*znorm(z+1) beta*(1-znorm(z+1))]) ;
+        tmax = max([-beta*znorm(z+1) beta*(1-znorm(z+1))]);
+        alpha_z = alpha + beta*znorm(z+1);
+        c_x = cmat(1,z+1);
+            % Find left x-value of boundary
+        funneg = @(t) fneg(t, alpha, beta, cmat, znorm, tmax, z);
+        left_tval = fzero(funneg, [tmin,0]);
+        left_xval = (tmax/(tmax-left_tval))*Logit(alpha_z + left_tval) - ...
             (left_tval/(tmax-left_tval))*Logit(alpha_z+tmax);
-        % Find right x-value of boundary
-    funpos = @(t) fpos(t, alpha, beta, cmat, znorm, tmin, z);
-    right_tval = fzero(funpos, [0,tmax]);
-    right_xval = (-tmin/(right_tval-tmin))*Logit(alpha_z + right_tval) + ...
+            % Find right x-value of boundary
+        funpos = @(t) fpos(t, alpha, beta, cmat, znorm, tmin, z);
+        right_tval = fzero(funpos, [0,tmax]);
+        right_xval = (-tmin/(right_tval-tmin))*Logit(alpha_z + right_tval) + ...
              (right_tval/(right_tval - tmin))*Logit(alpha_z + tmin);
-        % Horizontal distance between c and closest boundary
-    dist = min([c_x-left_xval,right_xval-c_x]);
+            % Horizontal distance between c and closest boundary
+        distvec(1,z+1) = c_x-left_xval;
+        distvec(2,z+1) = right_xval-c_x;
+    end
+    dist = min(distvec,[],"all");
 end
-
-
-
 
 function y=fneg(t, alpha, beta, cmat, znorm, tmax, z)
     alpha_z = alpha + beta*znorm(z+1);
@@ -68,7 +110,7 @@ function y = fpos(t, alpha, beta, cmat, znorm, tmin, z)
 end
 
 function y = Logit(x)
-    y = cdf('Logistic',x);
+    y = 1/(1+exp(-(x)));
 end
 
 
